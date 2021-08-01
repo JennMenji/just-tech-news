15645cb1ace1a980028c10608491aad719fa25ac
Menji <jennifer.menjivar18@gmail.com>
2021-08-01 15:28:04 -0400

    added sessions login/logout features

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ const path = require("path");
 // Set up handlebars
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
+const session = require("express-session");
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: "Super duper secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+}
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,6 +29,9 @@ app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+
+// middleware for sessions
+app.use(session(sess))
 
 // turn on routes
 app.use(routes);
